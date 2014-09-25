f767c977c18aa881f3b18862c899c13047030c82
App Academy Student <student@appacademy.io>
2014-09-25 18:13:46 -0400

    Start implementing SQLObject

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,26 +5,45 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    column_syms = DBConnection.execute(<<-SQL)
+      PRAGMA table_info(#{table_name})
+    SQL
+      .map{ |row| row["name"].to_sym }
+    column_syms.each do |sym|
+      define_method(sym) { attributes[sym] }
+
+      define_method("#{sym}="){ |value| attributes[sym] = value }
+    end
+
+    column_syms
   end
 
   def self.finalize!
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= name.pluralize.underscore
   end
 
   def self.all
-    # ...
+    parse_all(
+      DBConnection.execute(<<-SQL)
+        SELECT
+          *
+        FROM
+          #{table_name}
+      SQL
+    )
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |row|
+      new(row)
+    end
   end
 
   def self.find(id)
@@ -32,15 +51,22 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    @attributes = Hash.new
+    params.each do |key, value|
+      if self.class.columns.include?(key.to_sym)
+        @attributes[key.to_sym] = value
+      else
+        raise "unknown attribute '#{key}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes
   end
 
   def attribute_values
-    # ...
+    @attributes.values
   end
 
   def insert
